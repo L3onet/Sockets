71e392b858eca9316a6118eae83f6f4e21bf2ec6
Leonel Gonzalez Vidales <leonelgv@italtamirano.edu.mx>
2021-03-22 18:10:51 -0600

    Configuracion POST y CORS

--- Server/routes/router.ts
+++ Server/routes/router.ts
@@ -10,8 +10,27 @@ router.get('/mensajes' ,( req: Request, res: Response) => {
 });
 
 router.post('/mensajes' ,( req: Request, res: Response) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+
     res.json({
         ok: true,
-        mensaje: 'POST - ok'
+        cuerpo,
+        de
+    });
+});
+
+router.post('/mensajes/:id' ,( req: Request, res: Response) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+
+    res.json({
+        ok: true,
+        cuerpo,
+        de,
+        id
     });
 });
