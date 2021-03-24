059b96f67bf2cb8c63d7fcd7c821a8a961029f69
Leonel Gonzalez Vidales <leonelgv@italtamirano.edu.mx>
2021-03-23 21:32:00 -0600

    Configuracion de SocketIO con el backend

--- Server/classes/server.ts
+++ Server/classes/server.ts
@@ -1,14 +1,36 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
-
+import socketIO from 'socket.io';
+import http from 'http';
+import cors from 'cors';
 
 export default class Server {
     public app: express.Application;
     public port: number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
     constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server ( this.app );
+        this.io = require("socket.io")(this.httpServer,{
+            cors: {
+                origin: true,
+                credentials: true
+            },
+        });
+        this.escucharSockets();
+    }
+
+    private escucharSockets(){
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+        } );
     }
 
     start( callback: Function ){
